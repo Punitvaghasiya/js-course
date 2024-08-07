7f4f6f0ced71ae36a57ec9cac37d66b2e296bae5
Punitvaghasiya <punitvaghasiya4@gmail.com>
2024-08-07 06:14:03 +0000

    js variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1222
+let accountEmail = "punit@yahoo.com"
+var accountPassword = "12345"
+accountCity = "Junagadh"
+let accountState;
+
+// accountId = 4
+accountEmail = "ab@ab.com"
+accountPassword = "123456"
+accountCity = "Rajkot"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
