afe21cecb8d2642bcf60df4db23534be319ab432
Punitvaghasiya <punitvaghasiya4@gmail.com>
2024-08-09 04:40:17 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3 + 3) we are using nodejs, not browser
+
+console.log(3 
+    + 3); // code readability should be high
+
+console.log("ABC");
+
+let name = "Punit"
+let age = 19
+let isLoggedIn = false
+let state;
+
+// number => 2 power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefine
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefine
+console.log(typeof null); // object
+
+
+
+
+
