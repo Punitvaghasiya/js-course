db557d9a0563e2297a4102b5ab2f9cdcd4bda79e
Punitvaghasiya <punitvaghasiya4@gmail.com>
2024-08-12 05:46:01 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+// Primitive
+
+// 7 types: String, Number, Boolean, null, undefined, BigInt
+
+const score = 100
+const scoreValue = 100.5
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 23972529857239857293n
+
+
+
+// Reference (Non-Primitive)
+
+// Array, Objects, Functions
+
+const cars = ["Volvo", "BMW", "Thar"];
+
+let myObj = {
+    name: "Punit",
+    age: 19,
+}
+
+const myFunction = function(){
+    console.log("Hello world");   
+}
+
+console.log(typeof cars);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
