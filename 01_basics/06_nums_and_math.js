2fa36278d189f77b42f8de32993d36ec70b1f165
Punitvaghasiya <punitvaghasiya4@gmail.com>
2024-08-19 06:32:20 +0000

    math and num

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,4 +1,22 @@
 const score  = 400
+// console.log(score);
+
 
 const balance = new Number(100)
-console.log(balance);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round(4.3));
+
